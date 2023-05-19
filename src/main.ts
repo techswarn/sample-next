9fed5455fac922fd11784f444e53791e24d54b42
techswarn <tech.swarns@gmail.com>
2023-05-19 13:51:40 +0530

    code update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-console.log(process.env.HOST);
+console.log(`hello`);
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
